1c035e708e08d5da3b9f87b1121382ccf2c4ee2b
Eamonn Webster <eweb@qstream.com>
2017-03-11 11:04:57 +0000

    Graph lines missed as well as percent covered

--- lib/metric_fu/metrics/rcov/grapher.rb
+++ lib/metric_fu/metrics/rcov/grapher.rb
@@ -1,7 +1,7 @@
 MetricFu.reporting_require { "graphs/grapher" }
 module MetricFu
   class RcovGrapher < Grapher
-    attr_accessor :rcov_percent, :labels
+    attr_accessor :rcov_percent, :lines_missed, :labels
 
     def self.metric
       :rcov
@@ -10,12 +10,14 @@ module MetricFu
     def initialize
       super
       self.rcov_percent = []
+      self.lines_missed = []
       self.labels = {}
     end
 
     def get_metrics(metrics, date)
       if metrics && metrics[:rcov]
         rcov_percent.push(metrics[:rcov][:global_percent_run])
+        lines_missed.push(metrics[:rcov][:lines_missed] || 0)
         labels.update(labels.size => date)
       end
     end
@@ -26,7 +28,8 @@ module MetricFu
 
     def data
       [
-        ["rcov", @rcov_percent.join(",")]
+        ["rcov", @rcov_percent.join(",")],
+        ["missed", @lines_missed.join(",")]
       ]
     end
 
